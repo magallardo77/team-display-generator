5fd286e64172890dfde65c95b38cd20505582b48
MG <magallardo11@outlook.com>
2022-10-09 22:14:26 -0700

    questions running in js, and added older inquirer

--- index.js
+++ index.js
@@ -1,31 +1,105 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-questions = [
+managerQuestions = [
     {
         message: "What's the team managers name?",
         name: "managerName",
     },
     {
-        message: "What's your employee ID number?",
+        message: "What's your manager's ID number?",
         name: "employeeID",
     },
     {
-        message: "What's your email address?",
+        message: "What's your manager's email address?",
         name: "emailName",
     },
     {
-        message: "What's your office number?",
+        message: "What's your manager's office number?",
         name: "managerName",
     },
     {   
         type: "list",
-        message: "What role would you like to finish building out your team?",
+        message: "What role would you like to add to your team?",
         name: "roleName",
-        choices: ["Engineer", "Intern"]
-    },
-    {
-        message: "What's your office number?",
-        name: "managerName",
+        choices: ["Engineer", "Intern", "None, I am finished building my team."]
     },
 ]
+
+engineerQuestions = [
+    {
+        message: "What's the engineer's name?",
+        name: "managerName",
+    },
+    {
+        message: "What's your engineer's ID number?",
+        name: "employeeID",
+    },
+    {
+        message: "What's your engineer's email address?",
+        name: "emailName",
+    },
+    {
+        message: "What's your engineer's Github username?",
+        name: "managerName",
+    },
+    {   
+        type: "list",
+        message: "What role would you like to add to your team?",
+        name: "roleName",
+        choices: ["Engineer", "Intern", "None, I am finished building my team."]
+    },
+]
+
+internQuestions = [
+    {
+        message: "What's your intern's name?",
+        name: "managerName",
+    },
+    {
+        message: "What's your intern's ID number?",
+        name: "employeeID",
+    },
+    {
+        message: "What's your intern's email address?",
+        name: "emailName",
+    },
+    {
+        message: "What's your intern's school?",
+        name: "managerName",
+    },
+    {   
+        type: "list",
+        message: "What role would you like to add to your team?",
+        name: "roleName",
+        choices: ["Engineer", "Intern", "None, I am finished building my team."]
+    },
+]
+
+function init() {
+    inquirer.prompt(managerQuestions)
+        .then((answers) =>  {
+        if (answers.roleName === "Engineer") engineerPrompt();
+        else if (answers.roleName === "Engineer") internPrompt();
+        }
+        )
+}
+
+function engineerPrompt() {
+    inquirer.prompt(engineerQuestions)
+        .then((answers) =>  {
+            if (answers.roleName === "Engineer") engineerPrompt();
+            else if (answers.roleName === "Intern") internPrompt();
+        }
+        )
+}
+function internPrompt() {
+    inquirer.prompt(internQuestions)
+        .then((answers) =>  {
+            if (answers.roleName === "Engineer") engineerPrompt();
+            else if (answers.roleName === "Intern") internPrompt();
+        }
+        )
+}
+
+init()
